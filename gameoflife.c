e4ef9663cb9867061b4a88ec42546209d37b6e4b
Richtor Hartmann <ryuurei@cbvaio.(none)>
2011-05-07 16:52:47 -0230

    working version

--- gameoflife.c
+++ gameoflife.c
@@ -4,107 +4,95 @@
 #include <time.h>
 #include <SDL/SDL.h>
 
-#define ROWS 15
-#define COLS 15
+#define ROWS 50
+#define COLS 60
 #define OFF 0
 #define ON 1
 
-short number_of_neighbours(char ptarr[], short index);
-void update_board(char ptarr[]);
-void print_board(char ptarr[]);
+/** Note to self:
+  * Always refer to the board as board[x][y] as to follow the general
+  * standard for specifying coordinates. When looping, y must be declared
+  * first in the outer loop so that it represents the rows while x, declared
+  * within the y loop becomes the variable representing each column value.
+ */
+
+static char board[ROWS][COLS];
+
+void print_board(void);
+short num_neighbours(short x, short y);
+void update_board(void);
 
 int main(int argc, char* argv[]) {
-    char board[ROWS * COLS];
-    short num_neighbours;
+    short num_adj;
     short generator;
 
     srand(time(NULL));
-    memset((void*)board, OFF, ROWS*COLS); /* Set each cell to OFF. */
-    print_board(board);
-    for (short i = 0; i < (ROWS * COLS); i++) {
-        generator = (short)rand() % 3;
-        if (generator == 0)
-            board[i] = ON;
+    memset((void *)board, OFF, ROWS * COLS);
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            board[x][y] = OFF;
+        }
     }
-    printf("\n\n\n");
-    print_board(board);
-    for (short i = 0; i < 5; i++) {
-        SDL_Delay(6000);
-        update_board(board);
-        print_board(board);
-        printf("\n\n\n");
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            generator = (short)(rand() % 3);
+            if (generator == 0)
+                board[x][y] = ON;
+        }
+    }
+    print_board();
+    for (int i = 0; i < 15; i++) {
+        SDL_Delay(2500);
+        update_board();
+        print_board();
+        printf("\n\n");
     }
     return EXIT_SUCCESS;
 }
 
-short number_of_neighbours(char ptarr[], short index) {
+void print_board(void) {
+    printf("|");
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            if (board[x][y] == ON)
+                printf("X");
+            else
+                printf(" ");
+            printf("|");
+        }
+        if (y < (ROWS - 1)) printf("\n|");
+    }
+    printf("\n");
+}
+
+short num_neighbours(short x, short y) {
     short num_adj = 0;
-    
-    if (index < 0 || index > (ROWS * COLS))
-        return -1;
-    if (index - ROWS >= 0) {
-        if (ptarr[index - ROWS] == ON) {
-            num_adj++;
-            printf("Found block on above index %d.\n", index);
-        }
-    }
-    if (index + ROWS <= (ROWS * COLS)) {
-        if (ptarr[index + ROWS] == ON) {
-            num_adj++;
-            printf("Found block on right of index %d.\n", index);
-        }
-    }
-    if (index - 1 >= 0) {
-        if (ptarr[index - 1] == ON) {
-            num_adj++;
-            printf("Found block on left of index %d.\n", index);
-        }
-    }
-    if (index + 1 <= ROWS) {
-        if (ptarr[index + 1] == ON) {
-            num_adj++;
-            printf("Found block on below index %d.\n", index);
-        }
-    }
+
+    if (y-1 >= 0) 
+        if (board[x][y-1] == ON) num_adj++;
+    if (y+1 < ROWS)
+        if (board[x][y+1] == ON) num_adj++;
+    if (x-1 >= 0)
+        if (board[x-1][y] == ON) num_adj++;
+    if (x+1 < COLS)
+        if (board[x+1][y] == ON) num_adj++;
     return num_adj;
 }
 
-void update_board(char ptarr[]) {
-    short num_neighbours;
+void update_board(void) { 
+    short neighbours = 0;
 
-    for (int i = 0; i < (ROWS * COLS); i++) {
-        num_neighbours = number_of_neighbours(ptarr, i);
-        if (num_neighbours < 2 && ptarr[i] == ON) {
-            ptarr[i] = OFF; /* Cell dies by underpopulation. */
-            printf("Cell at index (%d, %d) dies.\n", (i / ROWS), (COLS - i));
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            neighbours = num_neighbours(x, y);
+            if (neighbours < 2 && board[x][y] == ON) {
+                board[x][y] = OFF; /* Dies by underpopulation. */
+            } else if (neighbours > 3 && board[x][y] == ON) {
+                board[x][y] = OFF; /* Dies by overpopulation. */
+            } else if (neighbours == 3 && board[x][y] == OFF) {
+                board[x][y] = ON; /* Becomes alive because of reproduction. */
+            }
+            /* Otherwise the cell lives with just the right company. */
         }
-        else if (num_neighbours > 3 && ptarr[i] == ON) {
-            ptarr[i] = OFF; /* Cell dies by overpopulation. */
-            printf("Cell at index (%d, %d) dies.\n", (i / ROWS), (COLS - i));
-        }
-        else if (num_neighbours == 3 && ptarr[i] == OFF) {
-            ptarr[i] = ON; /* Cell becomes alive by reproduction. */
-            printf("Cell at index (%d, %d) becomes alive.\n", (i / ROWS), (COLS -i));
-        }
-        else if ((num_neighbours == 2 && ptarr[i] == ON) ||
-                 (num_neighbours == 3 && ptarr[i] == ON)) {
-            ptarr[i] = ON;
-            printf("Cell at index (%d, %d) gets to live.\n", (i / ROWS), (COLS - i));
-        }
-    }
-}
-
-void print_board(char ptarr[]) {
-    printf("|");
-    for (short i = 0; i < (ROWS * COLS); i++) {
-        if (ptarr[i] == ON)
-            printf("X");
-        else
-            printf(" ");
-        printf("|");
-        if ((i % ROWS) == (COLS - 1))
-            printf("\n|");
-        /*else if ((i % ROWS) == 0)
-            printf("|"); */
     }
 }
